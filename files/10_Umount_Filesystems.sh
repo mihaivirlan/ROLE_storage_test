0519cc047ffc43db8d9374597acd41f99d976712
Thorsten Diehl <thorsten.diehl@de.ibm.com>
2021-02-03 18:32:49 +0100

    10_Umount_Filesystems.sh now computes the MOUNT_DIR

--- files/10_Umount_Filesystems.sh
+++ files/10_Umount_Filesystems.sh
@@ -2,7 +2,7 @@
 # Script-Name: 10_Umount_Filesystems.sh
 # Owner: Thomas Lambart
 # Date: 03. Feb. 2021
-# Description:  Perform a unmount_file_systems
+# Description:  Perform a unmount of file systems
 #
 #
 #
@@ -15,9 +15,19 @@ source ${TESTLIBDIR}lib/common/results.sh || exit 1
 source ${TESTLIBDIR}lib/common/remote.sh || exit 1
 source ${TESTLIBDIR}functions.sh || exit 1
 
+# determine the type of ATC to compute the variable MOUNT_DIR
+echo $TESTLIBDIR | grep SCSI_ && MOUNT_DIR="/mnt1" 
+echo $TESTLIBDIR | grep DASD_ && MOUNT_DIR="/mnt2" 
+echo $TESTLIBDIR | grep EDEV_ && MOUNT_DIR="/mnt3" 
+
 start_section 1 "Unmounting filesystems"
     echo "$0 is running with"
     echo "MOUNT_DIR  = $MOUNT_DIR"
     echo " "
-    unmountFilesystem
+    if [ "$MOUNT_DIR" != "" ]; then
+      unmountFilesystem
+    else
+      echo "No valid mount directory determined, exiting..."
+      exit 1;
+    fi
 end_section 1
