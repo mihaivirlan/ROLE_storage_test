e2020ec5351057076c60a3672dd3c3fda6b4ddc5
Thorsten Diehl <thorsten.diehl@de.ibm.com>
2021-02-03 18:04:01 +0100

    toybox sourcing removed; not needed

--- files/10_Umount_Filesystems.sh
+++ files/10_Umount_Filesystems.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # Script-Name: 10_Umount_Filesystems.sh
 # Owner: Thomas Lambart
-# Date: 14. Jan. 2021
+# Date: 03. Feb. 2021
 # Description:  Perform a unmount_file_systems
 #
 #
 #
-
-
 
 # Load testlib
 TESTLIBDIR="${TESTLIBDIR:-$(dirname $0)/}"
@@ -16,10 +14,6 @@ source ${TESTLIBDIR}lib/common/environment.sh || exit 1
 source ${TESTLIBDIR}lib/common/results.sh || exit 1
 source ${TESTLIBDIR}lib/common/remote.sh || exit 1
 source ${TESTLIBDIR}functions.sh || exit 1
-source "${TESTLIBDIR}lib/toybox/storage/libstorage.sh" || exit 1
-source "${TESTLIBDIR}lib/toybox/storage/libdasd.sh" || exit 1
-
-
 
 start_section 1 "Unmounting filesystems"
     echo "$0 is running with"
